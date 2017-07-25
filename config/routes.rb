fe602af198dcf8a41700a81ce14058cff1f1175b
Vincent Marie <vincent.marie@af83.com>
2017-07-25 16:42:19 +0200

    Add new indentation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #get 'index' => 'edwig_admin#index', as: 'EdwigAdmin'
-
   root "edwig_admin#index"
 end
