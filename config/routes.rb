80a5dae766690aade679111818bc37f318c480ea
Vincent Marie <vincent.marie@af83.com>
2017-07-25 11:59:32 +0200

    Initializing new rails app Edwig-admin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  	devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  	get 'index' => 'edwig_admin#index', as: 'EdwigAdmin'
+
+  	root "edwig_admin#index"
+end
