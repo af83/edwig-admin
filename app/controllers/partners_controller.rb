0e3843479b6e642183c909af938dd83a7272c319
Vincent Marie <vincent.marie@af83.com>
2017-07-28 10:52:17 +0200

    Add destroy_partner request for partners

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,17 @@
 class PartnersController < ApplicationController
   def index
+    @referential_slug = params[:referential_slug]
+    @referential_token = params[:token]
     partners = RestClient.get("#{Rails.configuration.edwig_api_host}/#{params[:referential_slug]}/partners", {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
     @partners_tab = JSON.parse(partners)
+  end 
+
+  def show
+
+  end
+
+  def destroy
+    destroy_partners = RestClient.delete("#{Rails.configuration.edwig_api_host}/#{params[:slug]}/partners/#{params[:id]}", {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
+    redirect_to referential_partners_path(referential_slug: params[:slug], token: params[:token])
   end
 end
