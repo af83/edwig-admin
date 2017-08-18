12614e7b482cda208d65857032bad2f3cb61159b
Vincent Marie <vincent.marie@af83.com>
2017-08-18 10:42:28 +0200

    Creation du local fr.yml

--- config/application.rb
+++ config/application.rb
@@ -13,3 +13,11 @@ module EdwigAdmin
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Whitelist locales available for the application
+I18n.available_locales = [:en, :fr]
+
+# Set default locale to something other than :en
+I18n.default_locale = :fr
