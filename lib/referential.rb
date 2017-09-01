7148d670dee599e600c9f7353a80e9e7abc5f030
Guillaume <guillaume.perot@af83.com>
2017-09-01 15:02:42 +0200

    fix referential/partner counting if null

--- lib/referential.rb
+++ lib/referential.rb
@@ -1,7 +1,7 @@
 class Referential
   attr_reader :id, :slug, :token, :next_reload_at, :partner
 
-  def initialize(id: nil, slug:, token:, next_reload_at: nil, partner:)
+  def initialize(id: nil, slug:, token:, next_reload_at: nil, partner: nil)
     @id = id
     @slug = slug
     @token = token
