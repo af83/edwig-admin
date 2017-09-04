6ae69f3dfccb9f6942a35d5ef84fe9c8c790dc6b
Alban Peignier <alban.peignier@af83.com>
2017-09-04 12:54:34 +0200

    Add edwig_server in ApplicationController configuration with config parameters. Refs #4378

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def edwig_server
+    @edwig_server ||= Edwig::Server.new Rails.configuration.edwig_api_host, token: Rails.configuration.edwig_token, debug: true
+  end
+
 end
