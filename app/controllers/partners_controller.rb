41b9f06f88a310960f611760dd6457e2d9c5d674
Guillaume <guillaume.perot@af83.com>
2017-09-04 16:38:07 +0200

    modify partner controller/view Refs #4378

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -4,11 +4,7 @@ require 'partner'
 
 class PartnersController < ApplicationController
   def index
-    @referential_slug = params[:referential_slug]
-    @referential_token = params[:token]
-
-    partners = RestClient.get("#{Rails.configuration.edwig_api_host}/#{params[:referential_slug]}/partners", {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
-    @partners_tab = JSON.parse(partners)
+    @partners = referential.partners
   end
 
   def show
@@ -16,48 +12,42 @@ class PartnersController < ApplicationController
   end
 
   def new
-    @partner = Partner.new(
-      slug: params[:partner_slug],
-      connector_types: params[:connector_types]
-    )
+    @partner = Edwig::Partner.new referential
   end
 
   def create
-    attributes =
-      {
-        slug: params[:partner_slug],
-        connectorTypes: [params[:connector_types]],
-        settings: {"remote_url" => params[:settings_remote_url], "remote_credential" => params[:settings_remote_credential], "local_credential" => params[:settings_local_credential], "remote_objectid_kind" => params[:settings_remote_objectid_kind]}
-      }
-    RestClient.post("#{Rails.configuration.edwig_api_host}/#{params[:referential_slug]}/partners", attributes.to_json, {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
-    redirect_to referential_partners_path(token: params[:token])
+    @partner = referential.create_partner params[:partner]
+
+    if @partner.valid?
+      redirect_to referential_partners_path(referential.id)
+    else
+      render :new
+    end
   end
 
   def edit
-    @partner = Partner.new(
-      id: params[:id],
-      slug: params[:partner_slug],
-      status: {"OperationnalStatus" => params[:partner_status_operationnal_status], "ServiceStartedAt" => params[:partner_status_service_started_at]},
-      connector_types: [params[:connector_types]],
-      token: params[:token]
-    )
+    @partner = referential.find_partner params[:id]
   end
 
   def update
-    attributes =
-      {
-        slug: params[:partner_slug],
-        connectorTypes: [params[:connector_types]],
-        PartnerStatus: {"OperationnalStatus" => params[:partner_status_operationnal_statusl], "ServiceStartedAt" => params[:partner_status_service_started_at]},
-        settings: {"remote_url" => params[:settings_remote_url], "remote_credential" => params[:settings_remote_credential], "local_credential" => params[:settings_local_credential], "remote_objectid_kind" => params[:settings_remote_objectid_kind]},
-        tokens: [params[:token]]
-      }
-    RestClient.put("#{Rails.configuration.edwig_api_host}/#{params[:referential_slug]}/partners/#{params[:id]}", attributes.to_json, {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
-    redirect_to referential_partners_path(token: params[:token])
+    @partner = referential.find_partner params[:id]
+
+    @partner.attributes = params[:partner]
+
+    if @partner.save
+      redirect_to referential_partners_path(referential.id)
+    else
+      render :edit
+    end
+  end
+
+  def referential
+    @referential ||= edwig_server.find_referential params[:referential_id]
   end
 
   def destroy
-    RestClient.delete("#{Rails.configuration.edwig_api_host}/#{params[:slug]}/partners/#{params[:id]}", {content_type: :json, :Authorization => "Token token=#{params[:token]}"})
-    redirect_to referential_partners_path(referential_slug: params[:slug], token: params[:token])
+    partner = referential.find_partner params[:id]
+    partner.destroy
+    redirect_to referential_partners_path(referential.id)
   end
 end
