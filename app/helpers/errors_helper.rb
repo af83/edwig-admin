63a9b0c9bede71082f861ebb206fba82b6e477b3
Guillaume <guillaume.perot@af83.com>
2017-09-07 12:47:35 +0200

    create partial for all form, update helper error

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,10 +1,10 @@
 module ErrorsHelper
 
   def errors_full_messages(errors)
-    content_tag :ul do
+    content_tag :ul, class: "list-group" do
       errors.map do |attribute, messages|
         messages.map do |message|
-          content_tag :li, "#{attribute}: #{message}"
+          content_tag :li, "#{attribute}: #{message}", class: "alert alert-danger"
         end.join
       end.join.html_safe
     end
