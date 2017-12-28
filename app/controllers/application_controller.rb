e618776fc1ba3ceb4534465ee64beae249511138
Alban Peignier <alban.peignier@af83.com>
2017-12-28 17:07:15 +0100

    Create UsersController. Refs #5439

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  protected
+
   def edwig_server
     @edwig_server ||= Edwig::Server.new Rails.configuration.edwig_api_host, token: Rails.configuration.edwig_token, debug: true
+  end
+
+  def after_invite_path_for(inviter, invitee)
+    users_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
       post :db_save, on: :collection
     end
   end
+
+  resources :users
 end
